243b0fd8ce09f1e4135c97a9650756cd765fbf2b
locky5 <daikevin19@yahoo.com>
2019-07-22 17:08:57 -0400

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,13 +3,88 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  renderCards = () => {
+
+    let sortedStocksByName = [...this.props.stocks].sort((a, b) =>
+    (a.name.toLowerCase() > b.name.toLowerCase()) ? 1: -1)
+
+    let sortedStocksByPrice = [...this.props.stocks].sort((a, b) => (a.price > b.price) ? 1: -1)
+
+    let techStocks = this.props.stocks.filter(stock => stock.type === "Tech")
+
+    let financeStocks = this.props.stocks.filter(stock => stock.type === "Finance")
+
+    let sportsWearStocks = this.props.stocks.filter(stock => stock.type === "Sportswear")
+
+    if (this.props.choice === "Tech") {
+      return techStocks.map(stock => {
+        return <Stock
+          name={stock.name}
+          price={stock.price}
+          id={stock.id} addToPortfolio={this.props.addToPortfolio}
+          foundStock={this.props.foundStock}
+          />
+        }
+      )
+    } else if (this.props.choice === "Finance") {
+      return financeStocks.map(stock => {
+        return <Stock
+          name={stock.name}
+          price={stock.price}
+          id={stock.id} addToPortfolio={this.props.addToPortfolio}
+          foundStock={this.props.foundStock}
+          />
+        }
+      )
+    } else if (this.props.choice === "Sportswear") {
+      return sportsWearStocks.map(stock => {
+        return <Stock
+          name={stock.name}
+          price={stock.price}
+          id={stock.id} addToPortfolio={this.props.addToPortfolio}
+          foundStock={this.props.foundStock}
+          />
+        }
+      )
+    } else if (this.props.alphabet) {
+      return sortedStocksByName.map(stock => {
+        return <Stock
+          name={stock.name}
+          price={stock.price}
+          id={stock.id} addToPortfolio={this.props.addToPortfolio}
+          foundStock={this.props.foundStock}
+          />
+        }
+      )
+    } else if (this.props.number) {
+        return sortedStocksByPrice.map(stock => {
+          return <Stock
+            name={stock.name}
+            price={stock.price}
+            id={stock.id} addToPortfolio={this.props.addToPortfolio}
+            foundStock={this.props.foundStock}
+            />
+          }
+        )
+    } else if (this.props.choice === "All"){
+      return this.props.stocks.map(stock => {
+        return <Stock
+          name={stock.name}
+          price={stock.price}
+          id={stock.id} addToPortfolio={this.props.addToPortfolio}
+          foundStock={this.props.foundStock}
+          />
+        }
+      )
+    }
+
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.renderCards()}
       </div>
     );
   }
