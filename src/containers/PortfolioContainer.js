ba095fd58f09e0a3d6f6161aa27ce74203a88c5f
github username <github email address>
2019-07-22 16:50:28 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,13 +3,21 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  showStock(){
+    return this.props.stocks.map(stock => {
+      return (
+        <ul onClick={() => this.props.sellStock(stock)}>
+          <li>{stock.name}</li>
+        </ul>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {this.props.stocks ? this.showStock() : null}
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -8,7 +8,9 @@ class StockContainer extends Component {
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.props.stocks.map(stock => {
+            return <Stock addStock={this.props.addStock} stock={stock}/>
+          })
         }
       </div>
     );
