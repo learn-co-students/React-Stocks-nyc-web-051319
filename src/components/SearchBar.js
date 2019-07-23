6327e06debc6f20093d1b6a7048d1f21acbe3d89
Mendel Stillerman <menstiler@gmail.com>
2019-07-23 12:01:41 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const handleChange = (event) => {
+    if (event.target.value === 'Alphabetically') {
+      props.sortByName()
+    } else if (event.target.value === 'Price') {
+      props.sortByPrice()
+    }
+  }
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.nameChecked} onClick={(event) => handleChange(event)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.priceChecked} onClick={(event) => handleChange(event)}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(event) => props.filterStocks(event)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
