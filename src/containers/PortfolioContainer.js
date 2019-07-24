86c5c74dadac4bc4f1ac2b8ce4a6746cf65bf76e
github username <github email address>
2019-07-24 16:25:34 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,19 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderPortfolio = () => {
+    const { portfolio, handleClick } = this.props
+    return portfolio.map(stock => {
+      return <Stock key={stock.id} stock={stock} handleClick={handleClick} />
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.renderPortfolio()
           }
       </div>
     );
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,19 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  renderStocks = ()=>{
+    const {stocks, handleClick} = this.props
+    return stocks.map(stock => {
+      return <Stock key={stock.id} stock={stock} handleClick={handleClick}/>
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.renderStocks()
         }
       </div>
     );
